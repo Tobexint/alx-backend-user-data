96b8335478b64d2bb6e0beeb37fc8be96b342174
tobexint <tobexint@gmail.com>
2023-03-09 20:41:12 +0000

    app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,7 +62,7 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None and \
-        not auth.session_cookie(request):
+       not auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
