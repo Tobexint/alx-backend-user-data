ec38726213185fc438e076e8708c9ae18c6f48c4
tobexint <tobexint@gmail.com>
2023-03-15 19:04:16 +0000

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -14,3 +14,23 @@ app = Flask(__name__)
 def index() -> str:
     """returns a JSON payload"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """this method registers new users -> POST /users"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": f"{email}",
+                "message": "user created"
+            }), 200
+    except Exception:
+        return jsonify({
+            "message": "email already registered"
+        }), 400
+
